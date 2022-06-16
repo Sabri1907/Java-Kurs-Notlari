8f300bef5747f5ec8124b78ac7e7ee19606ca864
Sabri Guvendi <guvendisabri@gmail.com>
2022-06-16 20:09:06 +0300

    16.06.2022

--- src/day03_scanner/C05_Scanner.java
+++ src/day03_scanner/C05_Scanner.java
@@ -1,14 +1,20 @@
 package day03_scanner;
-
 import java.util.Scanner;
-
 public class C05_Scanner {
     public static void main(String[] args) {
+        /*
+        Kullanicidan iki sayi alip
+        bu sayilarin carpimini yazdirin
+         */
+        //1.adim
         Scanner scan=new Scanner(System.in);
-        System.out.println("Lutfen 1. sayiyi giriniz: ");
+        // 2.adim
+        System.out.println("Lutfen ilk sayiyi girin");
+        // 3.adim
         double sayi1= scan.nextDouble();
-        System.out.println("Lutfen 2. sayiyi giriniz: ");
-        double sayi2=scan.nextDouble();
-        System.out.println("Girilen sayilarin carpimi: "+sayi1*sayi2);
+        // 2.sayi icin 2.ve 3. adimlari tekrarlariz
+        System.out.println("Lutfen 2.sayiyi giriniz");
+        double sayi2= scan.nextDouble();
+        System.out.println("Girilen sayilari carpimi : "+sayi1*sayi2);
     }
 }
--- src/day03_scanner/C07_Scanner.java
+++ src/day03_scanner/C07_Scanner.java
@@ -1,16 +1,14 @@
 package day03_scanner;
-
 import java.util.Scanner;
-
 public class C07_Scanner {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-
-        System.out.println("Lutfen isminizi, soyisminizi ve yasinizi giriniz \naralarda Enter tusuna basiniz: ");
-
-        String isim=scan.nextLine();
-        String soyIsim=scan.nextLine();
+        // bir onceki soruyu tek seferde kullanicidan tum bilgileri alarak yapiniz
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen isminizi,soyisminizi ve yasinizi giriniz \naralarda Enter tusuna basiniz");
+        String isim= scan.nextLine();
+        String soyisim=scan.nextLine();
         int yas=scan.nextInt();
-        System.out.println("Isim, Soyisim ve Yasiniz: "+isim+" "+ soyIsim+" "+yas);
+        // girilen bilgiler = seyfi capar 34  seklinde yazdirin
+        System.out.println("girilen bilgiler = "+isim+", "+soyisim+", "+yas);
     }
 }
