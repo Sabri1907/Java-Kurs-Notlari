f53d17149aba2a7699efb3c01466268e1d15f567
Sabri Guvendi <guvendisabri@gmail.com>
2022-06-16 23:23:21 +0300

    day04-16.06.2022

--- src/day04_dataCasting/C04_ExplicitNarrowing.java
+++ src/day04_dataCasting/C04_ExplicitNarrowing.java
@@ -6,5 +6,19 @@ public class C04_ExplicitNarrowing {
          int sayi1=50;
 
          short sayi2= (short) sayi1;
+         /*
+            Genis data turundeki degeri, dara data turundeki variable'a atamak istersek
+         Java genis data turundeki degerin, dar data turunun sinirlarina uyup uymayacagini calistirana kadar
+         bilemez. Ama Java risk almaz.
+            Riski sifira indirmek icin burada bir sorun olursa sorumlulugu kabul etmemizi bekler.
+         Bunun icin degerinin onune parantez icinde istedigimiz data turunu yazmamiz gerekir.
+
+             Bu riski ustlendigimizde 3 durum olusabilir;
+         1. Bizim degerimiz dar kalip sinirlarina uygun olursa hic bir kayip olmadan cast olur.
+         2. Double bir sayiyi int'a cast etmek gibi durumlarda data kaybi yasayabiliriz (Ondalik kismi)
+         3. Genis kaliptan degeri dar kaliba koydugumuzda, sinirlari asan durumlarda veri baskalasabilir.
+         Ornek: Byte'ta 127'den sonrasi -128'den 0'a dogru devam eder.
+
+          */
     }
 }
