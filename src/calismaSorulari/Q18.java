da69cc0a0a3bbcaead2f7b2e2613a6495b89e716
Sabri Guvendi <guvendisabri@gmail.com>
2022-07-11 23:58:00 +0300

    11.07.2022

--- src/calismaSorulari/Q18.java
+++ src/calismaSorulari/Q18.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class Q18 {
     public static void main(String[] args) {
 
+        /*
+        SORU: 20'ye tam bolunen sayilari yaziniz
+        */
+        int sayi=20;
+        for (int i = -sayi; i <=sayi ; i++) {
+            if (!(i==0) && sayi%i==0){
+                System.out.print(i+" ");
+            }
+
+        }
+
+        System.out.println("");
+        System.out.println("********** WHILE LOOP ILE YAPILISI**********");
+
+        int temp=-20;
+
+         while(temp<=20){
+             if (!(temp==0) && sayi%temp==0){
+                 System.out.print(temp+" ");
+             }
+
+             temp++;
+         }
 
     }
 }
