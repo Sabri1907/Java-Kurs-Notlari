54165f9a7598dcd0b1dea3d4ef28454b508a20f7
Sabri Guvendi <guvendisabri@gmail.com>
2022-07-24 19:41:23 +0300

    24.07.2022

--- src/practice08/Q09_SurviveMonkey.java
+++ src/practice08/Q09_SurviveMonkey.java
@@ -12,4 +12,42 @@ public class Q09_SurviveMonkey {
         int numberOfBananas =165, survivalDays = 1;
         boolean monkeyAlive = true;
      */
+
+    public static void main(String[] args) {
+
+        int numberOfBananas=165;
+
+        int survivalDays=1;
+
+        boolean monkeyAlive=true;
+
+        do {
+            monkeyAlive=true;
+
+            numberOfBananas-=4;
+            survivalDays++;
+
+
+        }while(numberOfBananas>=4);
+
+        System.out.println("Monkey can survive "+(survivalDays-1)+" days");
+
+        System.out.println("*********ELIF HOCA'NIN COZUMU************");
+
+        int numberOfBananas2=165;
+
+        int survivalDays2=1;
+
+        do {
+            System.out.println("maymunlar gunde 4 muz yer");
+            numberOfBananas-=4; //toplam muzdan 4 azalir
+            survivalDays++;//yasadigi gun sayisini 1 arttir
+            if(numberOfBananas<4){ //4 den az muz kalirsa
+                monkeyAlive = false; //maymun hayatta degil
+                System.out.println("bugun  "+ survivalDays + " .gun muz kalmadi, maymun sizlere omur..:( ");
+            }else
+                System.out.println("bugun " + survivalDays + " gun, maymun halen hayatta..:) ");
+        }while(monkeyAlive);
+        System.out.println("toplam yasadigi gun sayisi : " + (survivalDays-1));
+    }
 }
