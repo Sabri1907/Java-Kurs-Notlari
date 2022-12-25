42c0da90e61eb6b9a2e3ebe9010ff6c8bc67a7c6
Sabri Guvendi <guvendisabri@gmail.com>
2022-12-25 22:27:41 +0300

    25.12.2022

--- src/lamda_functional_programming/Fp01.java
+++ src/lamda_functional_programming/Fp01.java
@@ -1,8 +1,57 @@
 package lamda_functional_programming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fp01 {
+    /*============================ =>  LAMDA <= ===========================================================
+    1. Lambda (Junctional Programming) Java 8 ile kullanılmaya başlanmıştır.
+    2. Functıonal Programming'de "NE Yapılacak" (What to do) üzerine yoğunlaşılır.
+       Stractured Programming'de "Nasıl Yapılacak" (How to do) üzerine yoğunlaşılır.
+    3. Functıonal Programming Arrays ve Collections ile kullanılır.
+    4. "entrySet()" methodu ile Map, Set'e dönüştürülerek Functıonal Programming'de kullanılır.
+     */
+
     public static void main(String[] args) {
+        List<Integer> liste = new ArrayList<>();
+        liste.add(8);
+        liste.add(9);
+        liste.add(131);
+        liste.add(10);
+        liste.add(9);
+        liste.add(10);
+        liste.add(2);
+        liste.add(8);
+        System.out.println("liste = " + liste);
+
+        listElemanlariniYazdirStructured(liste);
+        System.out.println();
+        listElemanlariniYazdirFunctional(liste);
+
+
 
     }
+    // 1. Ardışık list elementlerini aynı satırda aralarında boşluk bırakarak yazdıran bir method oluşturun. (Structured)
+    // Şimdiye kadar öğrendiklerimizle yapacağız.
+
+    public static void listElemanlariniYazdirStructured(List<Integer> list){
+        for(Integer w:list){
+            System.out.print(w+" ");
+        }
+    }
+
+
+    // 2. Ardışık list elementlerini aynı satırda aralarında boşluk bırakarak yazdıran bir method oluşturun. (Functional)
+    // Lambda ile yapacağız.
+
+    public static void listElemanlariniYazdirFunctional(List<Integer> list){
+
+        list.stream().forEach(t-> System.out.print(t+" "));
+        // "t->" list'deki elementlerin yerine geçer.
+        // stream() methodu lambda için bize yeni methodlar kullanmamızı sağlar. Ve collection'daki elementleri akışa sunar.
+        // BAzı methodları stream()'sız bazı methodları da stream() ile kullanamayız.
+
+    }
+
 
 }
